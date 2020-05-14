cfcffc83dde8d95f974366851020e424f19648f7
Sultan Alsawaf <sultan@kerneltoast.com>
2020-05-14 15:16:48 -0700

    simple_lmk: Consider all positive adjs when finding victims
    We are allowed to kill any process with a positive adj, so we shouldn't
    exclude any processes with adjs greater than 999. This would present a
    problem with quirky applications that set their own adj score, such as
    stress-ng. In the case of stress-ng, it would set its adj score to 1000
    and thus exempt itself from being killed by Simple LMK. This shouldn't
    be allowed; any process with a positive adj, up to the highest positive
    adj possible (32767) should be killable.
    
    Reported-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -44,13 +44,9 @@ struct victim_info {
 };
 
 /* Pulled from the Android framework. Lower adj means higher priority. */
-static const short adj_prio[] = {
-	906, /* CACHED_APP_MAX_ADJ */
-	905, /* Cached app */
-	904, /* Cached app */
-	903, /* Cached app */
-	902, /* Cached app */
-	901, /* Cached app */
+static const unsigned short adjs[] = {
+	SHRT_MAX + 1, /* Include all positive adjs in the final range */
+	950, /* CACHED_APP_LMK_FIRST_ADJ */
 	900, /* CACHED_APP_MIN_ADJ */
 	800, /* SERVICE_B_ADJ */
 	700, /* PREVIOUS_APP_ADJ */
@@ -58,9 +54,11 @@ static const short adj_prio[] = {
 	500, /* SERVICE_ADJ */
 	400, /* HEAVY_WEIGHT_APP_ADJ */
 	300, /* BACKUP_APP_ADJ */
+	250, /* PERCEPTIBLE_LOW_APP_ADJ */
 	200, /* PERCEPTIBLE_APP_ADJ */
 	100, /* VISIBLE_APP_ADJ */
-	0    /* FOREGROUND_APP_ADJ */
+	50, /* PERCEPTIBLE_RECENT_FOREGROUND_APP_ADJ */
+	0 /* FOREGROUND_APP_ADJ */
 };
 
 static struct victim_info victims[MAX_VICTIMS];
@@ -89,7 +87,19 @@ static bool vtsk_is_duplicate(int vlen, struct task_struct *vtsk)
 	return false;
 }
 
-static unsigned long find_victims(int *vindex, short target_adj)
+static unsigned long get_total_mm_pages(struct mm_struct *mm)
+{
+	unsigned long pages = 0;
+	int i;
+
+	for (i = 0; i < NR_MM_COUNTERS; i++)
+		pages += get_mm_counter(mm, i);
+
+	return pages;
+}
+
+static unsigned long find_victims(int *vindex, unsigned short target_adj_min,
+				  unsigned short target_adj_max)
 {
 	unsigned long pages_found = 0;
 	int old_vindex = *vindex;
